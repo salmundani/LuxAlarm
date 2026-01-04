a6409dcb844648113e52353891fe83152604e4b4
Daniel Salmun <salmundani@gmail.com>
2026-01-04 09:13:06 -0300

    version bump 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.dsalmun.luxalarm"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
